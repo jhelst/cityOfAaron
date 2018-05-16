450f4c48a17b9f662009c466536daf88e4672d74
Jeff Helsten <jeff.helsten@gmail.com>
2018-05-15 20:27:35 -0600

    Update comments section on main()

--- cityOfAaron/src/cityofaaron/CityOfAaron.java
+++ cityOfAaron/src/cityofaaron/CityOfAaron.java
@@ -1,13 +1,14 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * The main() class file for the cityOfAaron project
+ * CIT-260
+ * Spring 2018
+ * Team Members: Jeff Helsten, Ramon Andrade, Carolina Perez
  */
 package cityofaaron;
 
 /**
  *
- * @author FeltA
+ * @author jhelst, carolmadella
  */
 public class CityOfAaron {
 
