f76f06f6ec6e2f61e2968b4c52f0479bf268ff9e
tudonetwork <ramondandrade@yahoo.com>
2018-05-17 22:12:46 +0200

    new file:   cityOfAaron/.classpath 	new file:   cityOfAaron/.gitignore 	new file:   cityOfAaron/.project 	new file:   cityOfAaron/src/cityofaaron/model/TeamMembers.java

---
+++ cityOfAaron/src/cityofaaron/model/TeamMembers.java
@@ -0,0 +1,36 @@
+package cityofaaron.model;
+
+public enum TeamMembers {
+	
+	
+	private String name;
+	private String title;
+	
+	private TeamMembers(String name, String title) {
+		this.name = name;
+		this.title = title;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	
+	
+	
+
+
+}
+
