2104b72cb7f87f0aaefb1d40b2dfa42284fd9e09
Jeff Helsten <jeff.helsten@gmail.com>
2018-05-31 19:51:08 -0600

    Added plantCrops() method and updated tests

--- cityOfAaron/test/cityofaaron/control/CropControlTest.java
+++ cityOfAaron/test/cityofaaron/control/CropControlTest.java
@@ -54,6 +54,8 @@ public class CropControlTest {
     @Test
     public void testBuyLand() {
         System.out.println("buyLand");
+        
+        System.out.println("test1 - valid amount 10");
         CropData cropData = new CropData();
         cropData.setWheatInStore(200);
         cropData.setPopulation(200);
@@ -65,6 +67,76 @@ public class CropControlTest {
         int expResult = 30;
         int result = instance.buyLand(landPrice, acresToBuy, cropData);
         assertEquals(expResult, result);
+        
+        System.out.println("test2 - input < 0");
+        acresToBuy = -5;
+        expResult = -1;
+        result = instance.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);
+        
+        System.out.println("test3 - not enough population");
+        acresToBuy = 5000;
+        cropData.setWheatInStore(5000000);
+        expResult = -1;
+        result = instance.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);    
+        
+        System.out.println("test4 - not enough wheatInStore");
+        acresToBuy = 1;
+        cropData.setWheatInStore(0);
+        expResult = -1;
+        result = instance.buyLand(landPrice, acresToBuy, cropData);
+        assertEquals(expResult, result);            
+        
+    }
+    
+        /**
+     * Test of plantCrops method, of class CropControl.
+     * Author: jhelst
+     */
+    @Test
+    public void testPlantCrops() {
+        System.out.println("plantCrops");
+        
+        System.out.println("test1 - less than 0");
+        CropData cropData = new CropData();
+        cropData.setAcresOwned(200);
+        cropData.setAcresPlanted(10);
+        cropData.setWheatInStore(300);
+        
+        int acresToPlant = -1;
+        CropControl instance = new CropControl();
+        int expResult = -1;
+        int result = instance.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        
+        System.out.println("test2 - acceptable amount 10");
+        
+        acresToPlant = 10;
+        expResult = 295;
+        result = instance.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        
+        System.out.println("test2 - acceptable amount 30");
+        cropData.setWheatInStore(300);
+        acresToPlant = 30;
+        expResult = 285;
+        result = instance.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);
+        
+        System.out.println("test3- not enough acres available");
+        
+        acresToPlant = 1000;
+        expResult = -1;
+        result = instance.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);   
+        
+                System.out.println("test3- not enough wheatInStore");
+        cropData.setWheatInStore(0);
+        acresToPlant = 1000;
+        expResult = -1;
+        result = instance.plantCrops(acresToPlant, cropData);
+        assertEquals(expResult, result);   
     }
 
     /**
