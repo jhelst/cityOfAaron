cb64bff75e57808f1ac38cf8cfc7b08b7601ed24
carolmadella <carolina.madella@gmail.com>
2018-06-07 15:43:36 -0500

    Added feedPeople() method and updated tests

---
+++ cityOfAaron/src/view/MainMenuView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+import java.util.Scanner;
+/**
+ *The MainMenuView class - part of the view layer
+ *Object of this class manages the main menu
+ * @author carolmadella
+ */
+public class MainMenuView {
+   Scanner keyboard = new Scanner(System.in); 
+    /**
+* The displayMenuView method
+* Purpose: displays the menu, gets the user's input, and does the * selected action
+* Parameters: none
+* Returns: none
+*/
+public void displayMenuView(){
+}    
+// Display the menu
+// Prompt the user and get the user’s input // Perform the desired action
+// Determine and display the next view
+}
